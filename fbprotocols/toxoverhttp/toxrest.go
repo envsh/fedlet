31989e99dff0c9c3a0e218893a0a42353587eb53
egitop <egitop@uroad.xyz>
2026-06-04 10:03:59 +0800

    fix load events url join

--- fbprotocols/toxoverhttp/toxrest.go
+++ fbprotocols/toxoverhttp/toxrest.go
@@ -2,8 +2,10 @@ package toxoverhttp
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 	"fmt"
@@ -32,7 +34,8 @@ func Start(info string) {
 
 ////
 
-var toxrest_url = "http://127.0.0.1:8181/api/events?after="
+// base URL, code appends /api/events?after=N at runtime
+var toxrest_url = "http://127.0.0.1:8181"
 
 type Event struct {
 	ID        uint64    `json:"event_id"`
@@ -48,7 +51,17 @@ func poll_toxrest() {
 	client := &http.Client{Timeout: 60 * time.Second}
 
 	for cnter := 0;; cnter++ {
-		url := toxrest_url + strconv.FormatUint(after, 10)
+		u, err := url.Parse(toxrest_url)
+		if err != nil {
+			log.Println("url parse error:", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		u = u.JoinPath("/api/events")
+		q := u.Query()
+		q.Set("after", strconv.FormatUint(after, 10))
+		u.RawQuery = q.Encode()
+		url := u.String()
 		// log.Println("poll_toxrest: GET after=", after)
 		resp, err := client.Get(url)
 		if err != nil {
@@ -65,13 +78,23 @@ func poll_toxrest() {
 			}
 		}
 
+		body, _ := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		log.Println("response body:", string(body))
+
 		var events []Event
-		if err := json.NewDecoder(resp.Body).Decode(&events); err != nil {
-			resp.Body.Close()
-			log.Println("decode error:", err)
+		if err := json.Unmarshal(body, &events); err != nil {
+			var errResp struct {
+				Error string `json:"error"`
+			}
+			if err2 := json.Unmarshal(body, &errResp); err2 == nil && errResp.Error != "" {
+				log.Println("server error:", errResp.Error)
+			} else {
+				log.Println("decode error:", err)
+			}
+			time.Sleep(5 * time.Second)
 			continue
 		}
-		resp.Body.Close()
 
 		if len(events) == 0 {
 			log.Println("no events (timeout)", after, cnter)
